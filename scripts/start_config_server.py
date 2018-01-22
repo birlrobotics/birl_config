7eaae36bb1a0bae05749dfdb9a3e70398264a93f
sklaw <sk.law.lsq@gmail.com>
2018-01-22 18:57:51 +0800

    add HMM config

--- scripts/start_config_server.py
+++ scripts/start_config_server.py
@@ -3,7 +3,7 @@
 import rospy
 
 from dynamic_reconfigure.server import Server
-from birl_config.cfg import MultiModalConfig 
+from birl_config.cfg import MultiModalConfig, HMMConfig
 
 def callback(config, level):
     rospy.loginfo("config: %s, level %s."%(config, level))
@@ -13,4 +13,5 @@ if __name__ == "__main__":
     rospy.init_node("start_birl_config_server", anonymous = True)
 
     srv = Server(MultiModalConfig, callback, namespace='/birl_config/modality_config')
+    srv = Server(HMMConfig, callback, namespace='/birl_config/introspection_model_config/HMM')
     rospy.spin()
