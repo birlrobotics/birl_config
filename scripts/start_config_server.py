968410f2c20e8186e1c10bb8ce914af440e45c40
sklaw <sk.law.lsq@gmail.com>
2018-01-24 22:30:03 +0800

    use one cfg file for hmmlearn

--- scripts/start_config_server.py
+++ scripts/start_config_server.py
@@ -3,7 +3,7 @@
 import rospy
 
 from dynamic_reconfigure.server import Server
-from birl_config.cfg import MultiModalConfig, HMMConfig
+from birl_config.cfg import MultiModalConfig, hmmlearnConfig
 
 def callback(config, level):
     rospy.loginfo("config: %s, level %s."%(config, level))
@@ -13,5 +13,5 @@ if __name__ == "__main__":
     rospy.init_node("start_birl_config_server", anonymous = True)
 
     srv = Server(MultiModalConfig, callback, namespace='/birl_config/modality_config')
-    srv = Server(HMMConfig, callback, namespace='/birl_config/introspection_model_config/HMM')
+    srv = Server(hmmlearnConfig, callback, namespace='/birl_config/introspection_model_config/HMM/hmmlearn')
     rospy.spin()
